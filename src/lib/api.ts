a0159eaa4285076f42195b546cc32078da6dd20f
Derock <derock@derock.dev>
2023-10-05 13:17:37 -0400

    fix: use window instead of process

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import { edenTreaty } from "@elysiajs/eden";
 import type { API } from "../server/index";
 
 export const api = edenTreaty<API>(
-  typeof process != "undefined"
+  typeof window === "undefined"
     ? `http://localhost:${process.env.PORT ?? 3000}`
     : window.location.origin
 );
